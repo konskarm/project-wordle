ae04ab4e4629ab13055557c2fc41a3bc4f52533d
Konstantinos Karmas <konskarm@gmail.com>
2023-12-09 14:09:24 +0100

    Remove unnecessary logs

--- src/components/GameOverBanner/GameOverBanner.js
+++ src/components/GameOverBanner/GameOverBanner.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 function GameOverBanner({outcome, totalGuesses, answer}) {
-    console.log(outcome);
     if (!['win', 'lose'].includes(outcome)) {
         return undefined;
     }
     const className = `${outcome === 'win' ? 'happy' : 'sad'} banner`;
-    console.log(className);
+
     return (
         <div className={className}>
             {outcome === 'win' ?
